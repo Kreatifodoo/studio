b8dec4f203af41c00afaa3266ff183b98de25c2c
Kiki Rizki <kiki@kreatifodoo.com>
2026-03-13 12:04:26 +0000

    Try fixing this error: `Runtime ReferenceError: Separator is not defined

--- src/components/pos/SettingsView.tsx
+++ src/components/pos/SettingsView.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from '@/components/ui/separator';
 import { 
   Store, 
   Printer, 
