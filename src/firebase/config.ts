11f103af43ae210a8850e92544ddbc303d316358
Kiki Rizki <kiki@kreatifodoo.com>
2026-03-14 17:23:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7362528866-2cbbd",
+  "appId": "1:862341190566:web:ecc2a2219e0c2c79f37567",
+  "apiKey": "AIzaSyBeP06M2d-he07HYC2M89Ki2UFepyU5nio",
+  "authDomain": "studio-7362528866-2cbbd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "862341190566"
+};
